a5f2901cb6cdd7f07f81c5226e42a7b768a624c1
Eddie Fong <fonged@oocl.com>
2018-11-19 16:41:23 +0800

    done

--- main/main.js
+++ main/main.js
@@ -1,9 +1,16 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar(a,b) {
+    return a%b;
 
-function caclSum() {};
+};
 
-function caclSumInConditon() {};
+function caclSum(numArray1) {
+    return sum = numArray1.reduce((a, b) => a + b, 0);
+};
+
+function caclSumInConditon(numArray1,a) {
+    return numArray1.filter(x=>(x<a)).reduce((a, b) => a + b, 0);
+};
 
 module.exports = {
     caclRemaindar,
